17d1d662345fd3e85052632b3dc2084170cb342c
Gulshat84 <gulshatik.nugumanova84@gmail.com>
2025-05-05 20:23:52 +0300

    Added styles for @media (max-width: 700px)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-// Изменяем стиль при прокрутке
+// Changing the style when scrolling
 window.addEventListener('scroll', () => {
   document.querySelector('.header').classList.toggle('scrolled', window.scrollY > 50);
 });
+
